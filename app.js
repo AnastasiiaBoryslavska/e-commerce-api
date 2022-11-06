0ed7b94a01a6d5cf8717ee3d13ec60ec99f5227a
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2022-11-06 15:03:49 -0500

    Basic express route, connected DB, basic routes

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log('E-Commerce API');
+require("dotenv").config();
+require("express-async-errors");
+
+//express
+const express = require("express");
+const app = express();
+
+//connect DB
+const connectDB = require("./db/connect");
+
+//middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.send("e-commerce api");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port} `);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
