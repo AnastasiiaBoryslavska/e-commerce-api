6416078eb4236cadbf77c8210d0a4dff7ba5ece0
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2022-11-08 17:28:54 -0500

    Refactoring Moved cookie method to authController

--- app.js
+++ app.js
@@ -5,31 +5,19 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
-//rest of packages
-const morgan = require("morgan");
-const cookieParser = require('cookie-parser')
-
 //connect DB
 const connectDB = require("./db/connect");
-
-//router
-const authRouter = require("./routes/authRoutes");
 
 //middleware
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.use(morgan("tiny"));
 app.use(express.json());
-app.use(cookieParser())
 
-
+//routes
 app.get("/", (req, res) => {
-  console.log(req.cookies)
   res.send("e-commerce api");
 });
-
-app.use("/api/v1/auth", authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
